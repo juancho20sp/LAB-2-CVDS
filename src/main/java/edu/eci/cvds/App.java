ee18e1e2e3d283e9d542a24f7c6a160bdde0e28c
Juan David Murillo <juancho20sp@gmail.com>
2021-08-19 09:07:32 -0500

    Members added

--- src/main/java/edu/eci/cvds/App.java
+++ src/main/java/edu/eci/cvds/App.java
@@ -4,10 +4,18 @@ package edu.eci.cvds;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+//  mvn exec:java -Dexec.mainClass=test.Main -Dexec.args="arg1 arg2 arg3"
+    public static void main( String[] args ) {
+        String output = "Hello %s!";
+        String name = "World";
+
+        if (args.length > 0) {
+            name = args[0];
+        }
+
+        System.out.println("\n\n");
+        System.out.println(String.format(output, name));
+        System.out.println("\n\n");
     }
 }
